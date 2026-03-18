038d444e88f0b83044199d09e878ffe605e074f5
bkorshunov <mrdrakula164@gmail.com>
2026-03-18 21:58:26 +0500

    visuals

---
+++ UralGameJam2026-Mantra/Assets/Scripts/Pointer/Hoverable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Hoverable : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _sprite;
+
+    public void SetHover(bool value)
+    {
+        if (value)
+        {
+            _sprite.color = Color.yellow;
+        }
+        else
+        {
+            _sprite.color = Color.white;
+        }
+    }
+}
---
+++ UralGameJam2026-Mantra/Assets/Scripts/Pointer/PointerHoverHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PointerHoverHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private Hoverable _display;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("!!!");
+        Debug.Log("!!!");
+        _display.SetHover(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _display.SetHover(false);
+    }
+}
