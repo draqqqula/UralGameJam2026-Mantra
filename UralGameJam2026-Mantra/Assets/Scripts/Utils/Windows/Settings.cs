b0d37ffddc2d6ea825279d5be0a381c480f87ee0
WaRDy <ivan.alchin5@mail.ru>
2026-03-19 19:35:55 +0500

    настройки музыки и звуков отдельно

--- UralGameJam2026-Mantra/Assets/Scripts/Utils/Windows/Settings.cs
+++ UralGameJam2026-Mantra/Assets/Scripts/Utils/Windows/Settings.cs
@@ -6,9 +6,12 @@ using UnityEngine.UI;
 public class Settings : MonoBehaviour, IService
 {
     [SerializeField] private Slider _soundsSlider;
+    [SerializeField] private Slider _musicSlider;
+    
     [SerializeField] private AudioMixer _audioMixer;
     
-    private float _previousVolume;
+    private float _previousSoundsVolume;
+    private float _previousMusicVolume;
 
     private void OnEnable()
     {
@@ -18,31 +21,45 @@ public class Settings : MonoBehaviour, IService
     public void Init()
     {
         _soundsSlider.value = PlayerPrefs.GetFloat("SoundsVolume", .7f);
-        _soundsSlider.onValueChanged.AddListener(OnSliderChanged);
+        _musicSlider.value = PlayerPrefs.GetFloat("MusicVolume", .7f);
+        
+        _soundsSlider.onValueChanged.AddListener(OnSoundsSliderChanged);
+        _musicSlider.onValueChanged.AddListener(OnMusicSliderChanged);
 
-        _audioMixer.SetFloat("Volume", Mathf.Lerp(-60, 10, _soundsSlider.value));
-        _previousVolume = _soundsSlider.value;
+        _audioMixer.SetFloat("SoundsVolume", Mathf.Lerp(-60, 10, _soundsSlider.value));
+        _audioMixer.SetFloat("MusicVolume", Mathf.Lerp(-60, 10, _musicSlider.value));
+        
+        _previousSoundsVolume = _soundsSlider.value;
+        _previousMusicVolume = _musicSlider.value;
     }
 
     public void Cancel()
     {
         ServiceLocator.Instance.GetService<PauseHandler>()?.StopPause();
-        _audioMixer.SetFloat("Volume", Mathf.Lerp(-60, 10,_previousVolume));
+        _audioMixer.SetFloat("SoundsVolume", Mathf.Lerp(-60, 10,_previousSoundsVolume));
+        _audioMixer.SetFloat("MusicVolume", Mathf.Lerp(-60, 10, _previousMusicVolume));
     }
 
     public void Save()
     {
         ServiceLocator.Instance.GetService<PauseHandler>()?.StopPause();
         PlayerPrefs.SetFloat("SoundsVolume", _soundsSlider.value);
+        PlayerPrefs.SetFloat("MusicVolume", _musicSlider.value);
     }
 
-    private void OnSliderChanged(float value)
+    private void OnSoundsSliderChanged(float value)
     {
-        _audioMixer.SetFloat("Volume", Mathf.Lerp(-60, 10,value));
+        _audioMixer.SetFloat("SoundsVolume", Mathf.Lerp(-60, 10,value));
+    }
+
+    private void OnMusicSliderChanged(float value)
+    {
+        _audioMixer.SetFloat("MusicVolume", Mathf.Lerp(-60, 10, value));
     }
 
     private void OnDestroy()
     {
-        _soundsSlider.onValueChanged.RemoveListener(OnSliderChanged);
+        _soundsSlider.onValueChanged.RemoveListener(OnSoundsSliderChanged);
+        _musicSlider.onValueChanged.RemoveListener(OnMusicSliderChanged);
     }
 }
