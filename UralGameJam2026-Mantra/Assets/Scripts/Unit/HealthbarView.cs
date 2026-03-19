51b30b279f8bb178ab2914a8f5386f337dc9b929
WaRDy <ivan.alchin5@mail.ru>
2026-03-19 22:45:59 +0500

    цифры урона/отхила

--- UralGameJam2026-Mantra/Assets/Scripts/Unit/HealthbarView.cs
+++ UralGameJam2026-Mantra/Assets/Scripts/Unit/HealthbarView.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,22 +9,58 @@ public class HealthbarView : MonoBehaviour
     [SerializeField] private Image _healthBar;
     [SerializeField] private Unit _unit;
 
+    [SerializeField] private float _textDuration;
+    [SerializeField] private TextMeshProUGUI _healthChangingText;
+    [SerializeField] private Color _damageColor;
+    [SerializeField] private Color _healColor;
+    
+    private Coroutine _textCoroutine;
+
     public void Init(Unit unit)
     {
         _unit = unit;
         
-        _unit.Health.OnTakeDamage += OnHealthChanged;
-        _unit.Health.OnHeal += OnHealthChanged;
+        _unit.Health.OnTakeDamage += OnDamaged;
+        _unit.Health.OnHeal += OnHeal;
     }
 
-    private void OnHealthChanged(float _)
+    private void OnDamaged(float value)
+    {
+        UpdateText("- " + value.ToString(), _damageColor);
+        OnHealthChanged();
+    }
+
+    private void OnHeal(float value)
+    {
+        UpdateText("+ " + value.ToString(), _healColor);
+        OnHealthChanged();
+    }
+    
+    private void OnHealthChanged()
     {
         _healthBar.fillAmount = _unit.Health.CurrentHealth / _unit.Health.MaxHealth;
+    }
+    
+    private void UpdateText(string text, Color color)
+    {
+        _healthChangingText.gameObject.SetActive(true);
+        _healthChangingText.text = text;
+        _healthChangingText.color = color;
+        
+        if (_textCoroutine != null) StopCoroutine(_textCoroutine);
+        _textCoroutine = StartCoroutine(TextCoroutine());
+    }
+
+    private IEnumerator TextCoroutine()
+    {
+        yield return new WaitForSeconds(_textDuration);
+        _healthChangingText.gameObject.SetActive(false);
+        _textCoroutine = null;
     }
 
     private void OnDestroy()
     {
-        _unit.Health.OnTakeDamage -= OnHealthChanged;
-        _unit.Health.OnHeal -= OnHealthChanged;
+        _unit.Health.OnTakeDamage -= OnDamaged;
+        _unit.Health.OnHeal -= OnHeal;
     }
 }
