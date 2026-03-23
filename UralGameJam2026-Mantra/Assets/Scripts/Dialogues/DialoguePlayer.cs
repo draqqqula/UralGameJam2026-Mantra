80cbdb37df33375beb421df5fee8dd57ad1b7cbc
WaRDy <ivan.alchin5@mail.ru>
2026-03-23 20:10:29 +0500

    фикс текста урона, продолжение диалога на пробел

--- UralGameJam2026-Mantra/Assets/Scripts/Dialogues/DialoguePlayer.cs
+++ UralGameJam2026-Mantra/Assets/Scripts/Dialogues/DialoguePlayer.cs
@@ -2,19 +2,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using Random = UnityEngine.Random;
 
 public class DialoguePlayer : MonoBehaviour, IService
 {
     [SerializeField] private DialogueGroup[] _dialogueGroups;
     [SerializeField] private DialogueDisplay _dialogueDisplay;
+    [SerializeField] private InputActionReference _continueInput;
     private PartyManager _partyManager;
+        
+    private bool _isActive = false;
         
     private Action _finishCallback;
 
     public void Init()
     {
         _partyManager = ServiceLocator.Instance.GetService<PartyManager>();
+        _continueInput.action.performed += OnContinuePerformed;
+    }
+    
+    private void OnContinuePerformed(InputAction.CallbackContext context)
+    {
+        FinishDialogue();
     }
     
     public void PlayDialogueWithChance(string groupKey, float dialogueChance, int speakerCount, Action finishCallback = null)
@@ -33,16 +43,20 @@ public class DialoguePlayer : MonoBehaviour, IService
     
     public void PlayDialogue(string groupKey, int speakerCount, Action finishCallback = null)
     {
-        var replica = GetReplica(groupKey);
+        if (_isActive) return;
         
+        var replica = GetReplica(groupKey);
         var members = _partyManager.EnemyParty.GetRandomMembers(speakerCount).Select(u => u.transform);
         _dialogueDisplay.ShowDialogue(replica, members);
         
         _finishCallback = finishCallback;
+        _isActive = true;
     }
 
     public void FinishDialogue()
     {
+        if (!_isActive) return;
+        
         _dialogueDisplay.HideDialogue();
         
         if (_finishCallback != null)
@@ -50,6 +64,7 @@ public class DialoguePlayer : MonoBehaviour, IService
             _finishCallback.Invoke();
             _finishCallback = null;
         }
+        _isActive = false;
     }
     
     public string GetReplica(string groupKey)
@@ -58,6 +73,11 @@ public class DialoguePlayer : MonoBehaviour, IService
         
         if (group == null) return null;
         return group.Replicas[Random.Range(0, group.Replicas.Length)];
+    }
+
+    private void OnDestroy()
+    {
+        _continueInput.action.performed -= OnContinuePerformed;
     }
 }
 
