4ea413da2e696fc5186c0d6a3e8d2908267622eb
bkorshunov <mrdrakula164@gmail.com>
2026-03-25 02:45:16 +0500

    добавлено свечение для хайлайта

---
+++ UralGameJam2026-Mantra/Assets/Scripts/Pointer/Target/ColorHighlightEffect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ColorHighlightEffect : HighlightEffect
+{
+    [SerializeField] private SpriteRenderer _sprite;
+    [SerializeField] private Color _color;
+
+    public override void SetActive(bool value)
+    {
+        if (value)
+        {
+            _sprite.color = _color;
+        }
+        else
+        {
+            _sprite.color = Color.white;
+        }
+    }
+}
---
+++ UralGameJam2026-Mantra/Assets/Scripts/Pointer/Target/GlowHighlightEffect.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GlowHighlightEffect : HighlightEffect
+{
+    [SerializeField] private SpriteRenderer _sprite;
+    [SerializeField] private Material _outlineMaterial;
+    [SerializeField] private Material _defaultMaterial;
+
+    public override void SetActive(bool value)
+    {
+        _sprite.material = value ? _outlineMaterial : _defaultMaterial;
+    }
+}
