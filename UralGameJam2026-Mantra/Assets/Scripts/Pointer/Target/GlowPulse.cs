67e9010ed7c096fb83dc91139e4015afd6efd433
bkorshunov <mrdrakula164@gmail.com>
2026-03-25 03:06:04 +0500

    glow pulsating

---
+++ UralGameJam2026-Mantra/Assets/Scripts/Pointer/Target/GlowPulse.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GlowPulse : MonoBehaviour
+{
+    [SerializeField] private Material _material;
+    [SerializeField] private AnimationCurve _curve;
+    [SerializeField] private float _defaultValue;
+    [SerializeField] private float _amplitude;
+    [SerializeField] private float _speed = 1f;
+
+    void Update()
+    {
+        _material.SetFloat("_OutlineRadius", _defaultValue + _amplitude * _curve.Evaluate(Time.time * _speed));
+    }
+}
