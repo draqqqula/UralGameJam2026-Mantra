debf72dad9bb8ed8a189dcb7946cfd68f001c90a
bkorshunov <mrdrakula164@gmail.com>
2026-03-26 00:12:53 +0500

    tween for display

--- UralGameJam2026-Mantra/Assets/Scripts/Pointer/Select/SelectionDisplay.cs
+++ UralGameJam2026-Mantra/Assets/Scripts/Pointer/Select/SelectionDisplay.cs
@@ -8,6 +8,7 @@ public class SelectionDisplay : MonoBehaviour
 {
     [SerializeField] private SelectableUnit _selectable;
     [SerializeField] private SpriteRenderer _sprite;
+    private Tween _cached;
 
     private void Start()
     {
@@ -16,21 +17,23 @@ public class SelectionDisplay : MonoBehaviour
 
     private void HandleStateChanged(SelectState state)
     {
+        _cached?.Kill();
+
         if (state == SelectState.Unavailable)
         {
-            _sprite.color = new Color(0, 0, 0, 0);
+            _cached = _sprite.DOFade(0, 0.4f).SetLink(gameObject);
         }
         else if (state == SelectState.Available)
         {
-            _sprite.color = Color.white;
+            _cached = _sprite.DOColor(Color.white, 0.4f).SetLink(gameObject);
         }
         else if (state == SelectState.Hover)
         {
-            _sprite.color = Color.yellow;
+            _cached = _sprite.DOColor(Color.yellow, 0.4f).SetLink(gameObject);
         }
         else if (state == SelectState.Selected)
         {
-            _sprite.color = Color.red;
+            _cached = _sprite.DOColor(Color.red, 0.4f).SetLink(gameObject);
         }
     }
 }
