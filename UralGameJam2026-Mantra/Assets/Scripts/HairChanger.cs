e68a5b34cc8fc03e62b1508ca2df003fb6c94e3d
bkorshunov <mrdrakula164@gmail.com>
2026-03-28 19:09:56 +0500

    Ranger Animations

---
+++ UralGameJam2026-Mantra/Assets/Scripts/HairChanger.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.U2D.Animation;
+
+public class HairChanger : MonoBehaviour
+{
+    [SerializeField] private SpriteResolver _resolver;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
