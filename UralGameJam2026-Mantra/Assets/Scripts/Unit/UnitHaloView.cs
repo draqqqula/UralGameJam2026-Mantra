2667af10cba30792da7f19c7859c214e24d1e2e9
WaRDy <ivan.alchin5@mail.ru>
2026-03-29 18:00:06 +0500

    привязка камеры и нимба к голове

--- UralGameJam2026-Mantra/Assets/Scripts/Unit/UnitHaloView.cs
+++ UralGameJam2026-Mantra/Assets/Scripts/Unit/UnitHaloView.cs
@@ -8,17 +8,20 @@ public class UnitHaloView : MonoBehaviour
     [SerializeField] private float _duration = .2f, _force = .8f, _magnitude = .2f;
 
     private Vector3 _originalScale;
+    private Transform _posPoint;
 
     private float _current, _max = 1;
 
-    private void Start()
+    public void Init(Transform posPoint)
     {
         _originalScale = _halo.transform.localScale;
+        _posPoint = posPoint;
     }
 
     private void Update()
     {
         _halo.transform.localScale = _originalScale * Mathf.Sin(Time.time * _force * _current) * _magnitude;
+        transform.position = _posPoint.position;
     }
 
     public void SetHalo(float current, float max)
