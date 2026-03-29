78c2294ba40c376e7a04287c923cb952ac2f4270
bkorshunov <mrdrakula164@gmail.com>
2026-03-29 19:40:07 +0500

    New rooms

--- UralGameJam2026-Mantra/Assets/Scripts/Environment/EnvironmentGenerator.cs
+++ UralGameJam2026-Mantra/Assets/Scripts/Environment/EnvironmentGenerator.cs
@@ -4,10 +4,19 @@ public class EnvironmentGenerator : MonoBehaviour, IService
 {
     [SerializeField] private Transform _origin;
     [SerializeField] private RoomPool _rooms;
+    [SerializeField] private GameObject _lastRoom;
     private GameObject _instance;
 
     public void CreateRandom()
     {
+        var isLastRoom = ServiceLocator.Instance.GetService<RoomsController>().IsLastRoom();
+
+        if (isLastRoom)
+        {
+            ReplaceRoom(_lastRoom);
+            return;
+        }
+
         var id = Random.Range(0, _rooms.Rooms.Count);
         ReplaceRoom(_rooms.Rooms[id]);
     }
